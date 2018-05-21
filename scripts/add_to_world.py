73be7b89be2833f18c22ccbe30f156fbb5020426
Marc Hanheide <marc@hanheide.net>
2018-05-21 10:55:25 +0000

    Rebase from 'debian/kinetic/rasberry_gazebo'

---
+++ scripts/add_to_world.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 13:55:50 2018
+
+@author: Adam Binch
+@email: adambinch@gmail.com
+"""
+################################################################################
+import copy
+
+
+class AddtoWorld(object):
+    
+    def __init__(self, world_d):
+        
+        world_d['sdf']['world']['model']=[] # creates a list to store the models
+        world_d['sdf']['world']['actor']=[] # creates a list to store the actors
+        self.world_d = world_d
+        
+        
+    def add_poles(self, pole_d, pole_poses, pole_count=0):
+        n = len(pole_poses)
+        for i in range(n):
+            pole_id = pole_count+i
+            cpy= pole_d['sdf']['model']
+            cpy['@name'] = u'pole_' + str(pole_id)
+            posstr= str("%.3f %.3f 0.691 0.0 0.0 0.0"%(pole_poses[i][0], pole_poses[i][1]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))      
+
+
+    def add_trays(self, tray_d, tray_poses, tray_length, tray_count=0):
+        n = len(tray_poses)
+        for i in range(n):
+            tray_id = tray_count+i
+            cpy= tray_d['sdf']['model']
+            cpy['@name'] = u'tray_' + str(tray_id)
+            posstr = str("%.3f %.3f 1.382 0.0 0.0 0.0"%(tray_poses[i][0], tray_poses[i][1]))
+            cpy['link']['pose']['#text'] = posstr
+            geomstr = str("%.3f 0.15 0.005"%(tray_length))
+            cpy['link']['collision']['geometry']['box']['size'] = geomstr
+            cpy['link']['visual']['geometry']['box']['size'] = geomstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))
+            
+            
+    def add_traysp2(self, trayp2_d, trayp2_poses, tray_length, trayp2_count=0):
+        n = len(trayp2_poses)
+        for i in range(n):
+            trayp2_id = trayp2_count+i
+            cpy= trayp2_d['sdf']['model']
+            cpy['@name'] = u'tray_p2_' + str(trayp2_id)
+            posstr = str("%.3f %.3f 1.424 1.5707 0.0 0.0"%(trayp2_poses[i][0], trayp2_poses[i][1]))
+            cpy['link']['pose']['#text'] = posstr
+            geomstr = str("%.2f 0.085 0.005"%(tray_length))
+            cpy['link']['collision']['geometry']['box']['size'] = geomstr
+            cpy['link']['visual']['geometry']['box']['size'] = geomstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))
+            
+            
+    def add_pot21(self, pot21_d, pot21_poses, pot21_count=0):
+        n = len(pot21_poses)
+        for i in range(n):
+            pot21_id = pot21_count+i
+            cpy= pot21_d['sdf']['model']
+            cpy['@name'] = u'pot21_' + str(pot21_id)
+            posstr = str("%.3f %.3f 1.382 1.5707 0.0 1.5707"%(pot21_poses[i][0], pot21_poses[i][1]))
+            cpy['pose'] = posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))
+            
+            
+    def add_pot9(self, pot9_d, pot9_poses, pot9_count=0):
+        n = len(pot9_poses)
+        for i in range(n):
+            pot9_id = pot9_count+i
+            cpy= pot9_d['sdf']['model']
+            cpy['@name'] = u'pot9_' + str(pot9_id)
+            posstr = str("%.3f %.3f 1.382 1.5707 0.0 1.5707"%(pot9_poses[i][0], pot9_poses[i][1]))
+            cpy['pose'] = posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))   
+            
+            
+    def add_pot1(self, pot1_d, pot1_poses, pot1_count=0):
+        n = len(pot1_poses)
+        for i in range(n):
+            pot1_id = pot1_count+i
+            cpy= pot1_d['sdf']['model']
+            cpy['@name'] = u'pot1_' + str(pot1_id)
+            posstr = str("%.3f %.3f 1.467 0.0 0.0 0.0"%(pot1_poses[i][0], pot1_poses[i][1]))
+            cpy['link']['pose']['#text'] = posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))
+            
+            
+    def add_plants(self, plant_d, plant_poses, plant_count=0):
+        n = len(plant_poses)
+        for i in range(n):
+            plant_id = plant_count+i
+            cpy= plant_d['sdf']['model']
+            cpy['@name'] = u'plant_' + str(plant_id)
+            posstr= str("%.3f %.3f %.3f 0.0 0.0 %.4f"%(plant_poses[i][0], plant_poses[i][1], 
+                                                       plant_poses[i][2], plant_poses[i][3]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))                  
+            
+            
+    def add_plant21(self, plant21_d, plant21_poses, plant21_count=0):
+        n = len(plant21_poses)
+        for i in range(n):
+            plant21_id = plant21_count+i
+            cpy= plant21_d['sdf']['model']
+            cpy['@name'] = u'plant21_' + str(plant21_id)
+            posstr = str("%.3f %.3f 1.3 1.5707 0.0 1.5707"%(plant21_poses[i][0]-0.15, plant21_poses[i][1]-0.15))
+            cpy['pose'] = posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))              
+            
+
+    def add_plant9(self, plant9_d, plant9_poses, plant9_count=0):
+        n = len(plant9_poses)
+        for i in range(n):
+            plant9_id = plant9_count+i
+            cpy= plant9_d['sdf']['model']
+            cpy['@name'] = u'plant9_' + str(plant9_id)
+            posstr = str("%.3f %.3f 1.475 1.5707 0.0 1.5707"%(plant9_poses[i][0]-0.15, plant9_poses[i][1]-0.15))
+            cpy['pose'] = posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))  
+
+            
+    def add_plant1(self, plant1_d, plant1_poses, plant1_count=0):
+        n = len(plant1_poses)
+        for i in range(n):
+            plant1_id = plant1_count+i
+            cpy= plant1_d['sdf']['model']
+            cpy['@name'] = u'plant1_' + str(plant1_id)
+            posstr = str("%.3f %.3f 1.467 1.5707 0.0 0.0"%(plant1_poses[i][0], plant1_poses[i][1]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))                           
+            
+
+    def add_arches(self, arch_d, arch_poses, arch_count=0):
+        n = len(arch_poses)
+        for i in range(n):
+            arch_id = arch_count+i
+            cpy= arch_d['sdf']['model']
+            cpy['@name'] = u'dummy_arch_' + str(arch_id)
+            posstr= str("%.3f %.3f 0.0 1.5707 0.0 1.5707"%(arch_poses[i][0], arch_poses[i][1]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))
+            
+            
+    def add_canopy10m(self, canopy10m_d, canopy10m_poses, canopy10m_count=0):
+        n = len(canopy10m_poses)
+        for i in range(n):
+            canopy10m_id = canopy10m_count+i
+            cpy= canopy10m_d['sdf']['model']
+            cpy['@name'] = u'canopy10m_' + str(canopy10m_id)
+            posstr= str("%.3f %.3f 0.0 1.5707 0.0 1.5707"%(canopy10m_poses[i][0], canopy10m_poses[i][1]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))  
+            
+
+    def add_canopy4m(self, canopy4m_d, canopy4m_poses, canopy4m_count=0):
+        n = len(canopy4m_poses)
+        for i in range(n):
+            canopy4m_id = canopy4m_count+i
+            cpy= canopy4m_d['sdf']['model']
+            cpy['@name'] = u'canopy4m_' + str(canopy4m_id)
+            posstr= str("%.3f %.3f 0.0 1.5707 0.0 1.5707"%(canopy4m_poses[i][0], canopy4m_poses[i][1]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))  
+            
+            
+    def add_canopyhalfm(self, canopyhalfm_d, canopyhalfm_poses, canopyhalfm_count=0):
+        n = len(canopyhalfm_poses)
+        for i in range(n):
+            canopyhalfm_id = canopyhalfm_count+i
+            cpy= canopyhalfm_d['sdf']['model']
+            cpy['@name'] = u'canopyhalfm_' + str(canopyhalfm_id)
+            posstr= str("%.3f %.3f 0.0 1.5707 0.0 1.5707"%(canopyhalfm_poses[i][0], canopyhalfm_poses[i][1]))
+            cpy['pose']=posstr
+            self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))     
+            
+            
+    def add_riseholme_enclosure(self, riseholme_enclosure_d, pose, j=0):
+        cpy= riseholme_enclosure_d['sdf']['model']
+        cpy['@name'] = u'riseholme_enclosure_' + str(j)
+        posstr= str("%.3f %.3f 0.0 0.0 0.0 0.0"%(pose[0], pose[1]))
+        cpy[u'pose'][u'#text'] = posstr
+        self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))     
+        
+        
+    def add_fhs(self, fhs_d, pose, j=0):
+        cpy= fhs_d['sdf']['model']
+        cpy['@name'] = u'food_handling_shed_' + str(j)
+        posstr= str("%.3f %.3f 0.0 0.0 0.0 0.0"%(pose[0], pose[1]))
+        cpy[u'pose'][u'#text'] = posstr
+        self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))     
+        
+        
+    def add_fhs_floor(self, fhs_floor_d, pose, j=0):
+        cpy= fhs_floor_d['sdf']['model']
+        cpy['@name'] = u'fhs_floor_' + str(j)
+        posstr= str("%.3f %.3f 0.01 0.0 0.0 0.0"%(pose[0], pose[1]))
+        cpy['link']['pose']['#text'] = posstr
+        self.world_d['sdf']['world']['model'].append(copy.deepcopy(cpy))
+        
+    
+    def add_actors(self, actor_d, actor):    
+        cpy_actor = copy.deepcopy(actor_d['actor'])
+        cpy_actor['@name'] = actor['name']
+        cpy_actor['animation']['filename'] = actor['animation']
+        cpy_actor['script']['trajectory']['waypoint'] = []
+        for waypoint in actor['waypoints']:
+            d = {'pose': '', 'time': ''}
+            d['time'] = waypoint['time']
+            d['pose'] = waypoint['pose'] 
+            cpy_actor['script']['trajectory']['waypoint'].append(copy.deepcopy(d))    
+        self.world_d['sdf']['world']['actor'].append(copy.deepcopy(cpy_actor))        
+################################################################################            
---
+++ scripts/generate_world.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 11:36:00 2018
+
+@author: Adam Binch
+@email: adambinch@gmail.com
+"""
+################################################################################
+import argparse, xmltodict, os, rospkg
+from add_to_world import AddtoWorld
+from utils import load_data_from_yaml, load_data_from_xml
+from generate_poses import *
+
+
+save_world = True
+
+rospack = rospkg.RosPack()
+base_dir = rospack.get_path('rasberry_gazebo')
+#base_dir = os.getcwd()[:-8]
+
+world_f = base_dir + '/worlds/empty_grass.world'
+world_d = load_data_from_xml(world_f)
+world = AddtoWorld(world_d)
+world_name = 'thorvald_AB.world' # default
+
+default_model_config = base_dir + '/config/models_AB.yaml'
+default_actor_config = base_dir + '/config/actors_AB.yaml'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_file", type=str, default=default_model_config, help="filename")       
+parser.add_argument("--actor_file", type=str, default=default_actor_config, help="filename")
+args = parser.parse_args()
+model_config = load_data_from_yaml(args.model_file)
+actor_config = load_data_from_yaml(args.actor_file)
+################################################################################
+
+
+################################################################################
+# MODELS
+
+
+for model in model_config:
+    
+    if model.keys()[0] == 'polytunnel':
+        pole_f = base_dir + '/models/pole/model.sdf'
+        tray_f = base_dir + '/models/tray/model.sdf'
+        trayp2_f = base_dir + '/models/tray_part2/model.sdf'
+        pot21_f = base_dir + '/models/pot21/model.sdf'
+        pot9_f = base_dir + '/models/pot9/model.sdf'
+        pot1_f = base_dir + '/models/pot1/model.sdf'
+        #plant_f = base_dir + '/models/plant/model.sdf'
+        plant21_f = base_dir + '/models/plant21/model.sdf'
+        plant9_f = base_dir + '/models/plant9/model.sdf'
+        plant1_f = base_dir + '/models/plant1/model.sdf'
+        arch_f = base_dir + '/models/dummy_arch/model.sdf'
+        canopy10m_f = base_dir + '/models/canopy10m/model.sdf'
+        canopy4m_f = base_dir + '/models/canopy4m/model.sdf'
+        canopyhalfm_f = base_dir + '/models/canopyhalfm/model.sdf'
+        
+        
+        pole_d = load_data_from_xml(pole_f)    
+        tray_d = load_data_from_xml(tray_f)    
+        trayp2_d = load_data_from_xml(trayp2_f)
+        pot21_d = load_data_from_xml(pot21_f)
+        pot9_d = load_data_from_xml(pot9_f)
+        pot1_d = load_data_from_xml(pot1_f)
+        #plant_d = load_data_from_xml(plant_f)
+        plant21_d = load_data_from_xml(plant21_f)
+        plant9_d = load_data_from_xml(plant9_f)
+        plant1_d = load_data_from_xml(plant1_f)
+        arch_d = load_data_from_xml(arch_f)
+        canopy10m_d = load_data_from_xml(canopy10m_f)        
+        canopy4m_d = load_data_from_xml(canopy4m_f)        
+        canopyhalfm_d = load_data_from_xml(canopyhalfm_f) 
+        
+        
+        pole_count = 0
+        tray_count = 0
+        trayp2_count = 0
+        pot21_count = 0
+        pot9_count = 0
+        pot1_count = 0
+        #plant_count = 0
+        plant21_count = 0
+        plant9_count = 0
+        plant1_count = 0
+        arch_count = 0
+        canopy10m_count = 0   
+        canopy4m_count = 0   
+        canopyhalfm_count = 0
+        
+        
+        polytunnels = model['polytunnel']
+        for i in range(len(polytunnels)):
+            if polytunnels[i]['include']:            
+            
+                pole_nx = polytunnels[i]['pole_nx']
+                pole_ny = polytunnels[i]['pole_ny']
+                pole_dx = polytunnels[i]['pole_dx']
+                pole_dy = polytunnels[i]['pole_dy']
+                pole_xoffset = polytunnels[i]['pole_xoffset']
+                pole_yoffset = polytunnels[i]['pole_yoffset']
+                
+                arch_nx = polytunnels[i]['arch_nx']
+                arch_dx = polytunnels[i]['arch_dx']
+                arch_xoffset = polytunnels[i]['arch_xoffset']
+                
+                
+            
+                if (pole_nx * pole_dx) > 0 and (pole_ny * pole_dy) > 0:
+                    pole_poses, pole_xposes, pole_yposes = get_pole_poses(pole_nx, pole_ny, pole_dx, pole_dy, pole_xoffset, pole_yoffset) 
+                    world.add_poles(pole_d, pole_poses, pole_count)
+                    pole_count += len(pole_poses)
+                
+                    tray_poses, tray_length = get_tray_poses(pole_nx, pole_ny, pole_dx, pole_xoffset, pole_yposes)
+                    world.add_trays(tray_d, tray_poses, tray_length, tray_count) 
+                    tray_count += len(tray_poses)
+                
+                    trayp2_poses = get_trayp2_poses(tray_length, pole_xoffset, pole_ny, pole_yposes)
+                    world.add_traysp2(trayp2_d, trayp2_poses, tray_length, trayp2_count)                 
+                    trayp2_count += len(trayp2_poses)
+                    
+                    pot21_poses, pot21_max_length = get_pot21_poses([pole_xposes[0], pole_xposes[-1]], pole_yposes)
+                    world.add_pot21(pot21_d, pot21_poses, pot21_count)  
+                    pot21_count += len(pot21_poses)
+                
+                    pot9_poses, pot9_max_length = get_pot9_poses([pole_xposes[0], pole_xposes[-1]], pole_yposes, pot21_max_length)
+                    world.add_pot9(pot9_d, pot9_poses, pot9_count)  
+                    pot9_count += len(pot9_poses)
+                    
+                    pot1_poses, pot1_xposes = get_pot1_poses([pole_xposes[0], pole_xposes[-1]], pole_yposes, pot21_max_length, pot9_max_length) 
+                    world.add_pot1(pot1_d, pot1_poses, pot1_count)
+                    pot1_count += len(pot1_poses)
+                
+                    #plant_poses = get_plant_poses(pot1_xposes, pole_yposes)
+                    #world.add_plants(plant_d, plant_poses, plant_count)  
+                    #plant_count += len(plant_poses)
+                    
+                    world.add_plant21(plant21_d, pot21_poses, plant21_count)  
+                    plant21_count += len(pot21_poses)                    
+                    
+                    world.add_plant9(plant9_d, pot9_poses, plant9_count)  
+                    plant9_count += len(pot9_poses)
+                    
+                    world.add_plant1(plant1_d, pot1_poses, plant1_count)  
+                    plant1_count += len(pot1_poses)
+
+
+                if (arch_nx * arch_dx) > 0:
+                    arch_poses, arch_xposes, arch_yposes = get_arch_poses(arch_nx, arch_dx, arch_xoffset, pole_dy, pole_yposes)
+                    world.add_arches(arch_d, arch_poses, arch_count)
+                    arch_count += len(arch_poses)    
+                    
+                    canopy10m_poses, canopy10m_max_length = get_canopy10m_poses([arch_xposes[0], arch_xposes[-1]], arch_yposes[0], pole_dy)
+                    world.add_canopy10m(canopy10m_d, canopy10m_poses, canopy10m_count)
+                    canopy10m_count += len(canopy10m_poses)
+                    
+                    canopy4m_poses, canopy4m_max_length = get_canopy4m_poses([arch_xposes[0], arch_xposes[-1]], arch_yposes[0], pole_dy, canopy10m_max_length)
+                    world.add_canopy4m(canopy4m_d, canopy4m_poses, canopy4m_count)
+                    canopy4m_count += len(canopy4m_poses)
+
+                    canopyhalfm_poses = get_canopyhalfm_poses([arch_xposes[0], arch_xposes[-1]], arch_yposes[0], pole_dy, canopy10m_max_length, canopy4m_max_length)
+                    world.add_canopyhalfm(canopyhalfm_d, canopyhalfm_poses, canopyhalfm_count)
+                    canopyhalfm_count += len(canopyhalfm_poses)    
+   
+   
+   
+    if model.keys()[0] == 'riseholme_enclosure':
+        riseholme_enclosure_f = base_dir + '/models/riseholme_enclosure/model.sdf'    
+        riseholme_enclosure_d = load_data_from_xml(riseholme_enclosure_f)
+        
+        riseholme_enclosure = model['riseholme_enclosure']
+        for i in range(len(riseholme_enclosure)):
+            if riseholme_enclosure[i]['include']:
+                
+                xpose = riseholme_enclosure[i]['xpose']
+                ypose = riseholme_enclosure[i]['ypose']
+                world.add_riseholme_enclosure(riseholme_enclosure_d, [xpose, ypose], i)
+                
+                
+                
+    if model.keys()[0] == 'food_handling_shed':
+        fhs_f = base_dir + '/models/food_handling_shed/model.sdf'  
+        fhs_floor_f = base_dir + '/models/fhs_floor/model.sdf' 
+        fhs_d = load_data_from_xml(fhs_f)
+        fhs_floor_d = load_data_from_xml(fhs_floor_f)
+        
+        fhs = model['food_handling_shed']
+        for i in range(len(fhs)):
+            if fhs[i]['include']:
+                
+                xpose = fhs[i]['xpose']
+                ypose = fhs[i]['ypose']
+                world.add_fhs(fhs_d, [xpose, ypose], i)
+                world.add_fhs_floor(fhs_floor_d, [xpose, ypose], i)
+                
+                            
+                            
+    if model.keys()[0] == 'world_name':  
+        world_name = model['world_name']              
+################################################################################
+
+
+
+################################################################################        
+# ACTORS
+
+
+actor_f = base_dir + '/scripts/actor'
+actor_d = load_data_from_xml(actor_f)
+for actor in actor_config:
+    if actor['include']:
+        world.add_actors(actor_d, actor)
+################################################################################
+
+
+
+################################################################################        
+m = world.world_d['sdf']['world']['model']
+for i in range(len(m)):
+    print "-----------------"
+    print "[%s]"%m[i]['@name']
+    
+a = world.world_d['sdf']['world']['actor']
+for i in range(len(a)):
+    print "-----------------"
+    print "[%s]"%a[i]['@name']    
+
+if save_world:
+    fh = open(base_dir + '/worlds/' + world_name, "w")
+    s_output = xmltodict.unparse(world.world_d, pretty=True)
+    fh.write(s_output)
+    fh.close()    
+################################################################################
