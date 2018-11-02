60b89032592fcfd647d7fe481c9e789359cb9cdc
LCAS build farm <lcas-build-farm@googlegroups.com>
2018-11-02 10:08:23 +0000

    Trimmed the branch to only the rasberry_people_perception sub directory

--- rasberry_bringup/tmule/riseholme-uv.sh
+++
@@ -1,4 +0,0 @@
-export TMAP="riseholme-uv"
-export MAP="`readlink -f ../../rasberry_navigation/maps/riseholme.yaml`"
-export NOGOMAP="`readlink -f ../../rasberry_navigation/maps/riseholme_no_go.yaml`"
-export USE_CARROT=true
