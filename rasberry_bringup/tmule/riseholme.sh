34d49908adf7236a6a0fafd2057b48d8bbc22dfc
LCAS build farm <lcas-build-farm@googlegroups.com>
2018-11-02 10:08:31 +0000

    Trimmed the branch to only the rasberry_uv sub directory

--- rasberry_bringup/tmule/riseholme.sh
+++
@@ -1,5 +0,0 @@
-export TMAP="riseholme"
-export MAP="`readlink -f ../../rasberry_navigation/maps/riseholme.yaml`"
-export NOGOMAP="`readlink -f ../../rasberry_navigation/maps/riseholme_no_go.yaml`"
-export ROSBRIDGE_IP="10.8.0.18"
-
--- rasberry_bringup/tmule/sim_riseholme-uv.sh
+++
@@ -1,8 +0,0 @@
-export TMAP="riseholme-uv_sim"
-export MAP="`readlink -f ../../rasberry_navigation/maps/riseholme_sim.yaml`"
-export NOGOMAP="`readlink -f ../../rasberry_navigation/maps/riseholme_sim_no_go.yaml`"
-export USE_SIM=true
-export USE_GUI=false
-export USE_IMU=false
-export EKF_PUBLISH_TF=false
-export USE_CARROT=true
